1a647d50369ce33ee97ae820540c6e857fd0b720
LI-YONG-QI <a2369028@gmail.com>
2023-06-18 21:03:11 +0800

    fix: add notes field and validate function

--- app.ts
+++ app.ts
@@ -5,15 +5,34 @@ import { Request, Response } from "express";
 const app = express();
 app.use(express.json());
 
-interface restaurantData {
+interface RestaurantData {
   name: string;
   mapUrl: string;
+  notes?: string;
+}
+
+interface RestaurantFilter {
+  id?: string;
+  name?: string;
 }
 
 const PORT = process.env.PORT || 3000;
 
+const validateIsExist = async (filter: RestaurantFilter): Promise<boolean> => {
+  const result = await prisma.restaurant.findUnique({ where: filter });
+  return result ? true : false;
+};
+
+const validateParams = (params: any): params is RestaurantData => {
+  return (
+    typeof params.name === "string" &&
+    typeof params.mapUrl === "string" &&
+    (typeof params.notes === "string" || typeof params.notes === "undefined")
+  );
+};
+
 //** Get the list of restaurants, return empty array if no data
-app.get("/restaurants", async (req, res) => {
+app.get("/restaurants", async (req: Request, res: Response) => {
   const allRestaurants = await prisma.restaurant.findMany();
   res.status(200).json({ data: allRestaurants });
 });
@@ -23,11 +42,11 @@ app.get("/restaurants/:id", async (req, res) => {
   const { id } = req.params;
   const specificRes = await prisma.restaurant.findUnique({
     where: {
-      id: id,
+      id,
     },
   });
   if (!specificRes) {
-    return res.status(404).json({ message: "Could not find restaurant." });
+    return res.status(404).json();
   }
   res.status(200).json({ data: specificRes });
 });
@@ -36,73 +55,76 @@ app.get("/restaurants/:id", async (req, res) => {
 // 1. If the restaurant name already exists, return 409 conflicts
 // 2. If the given information did not pass the validation, return 400 bad requests
 app.post("/restaurants", async (req: Request, res: Response) => {
-  const { name, mapUrl } = req.body as { name: string; mapUrl: string };
+  const { name, mapUrl, notes } = req.body;
 
-  if (typeof name !== "string" || typeof mapUrl !== "string") {
-    return res.status(400).json({ message: "Could not update restaurant" });
+  if (!validateParams(req.body)) {
+    return res.status(400).json();
   }
 
-  if (await prisma.restaurant.findUnique({ where: { name } })) {
-    return res.status(409).json({ message: "Restaurant already exists" });
+  const isNameExist = await validateIsExist({ name });
+
+  if (!isNameExist) {
+    const newRestaurant = await prisma.restaurant.create({
+      data: {
+        name,
+        mapUrl,
+        notes,
+      },
+    });
+    return res.status(201).json({ data: newRestaurant });
+  } else {
+    return res.status(409).json();
   }
-
-  const newRestaurant = await prisma.restaurant.create({
-    data: {
-      name,
-      mapUrl,
-    },
-  });
-
-  if (!newRestaurant) {
-    return res.status(400).json({ message: "Could not create restaurant." });
-  }
-
-  res.status(201).json({ data: newRestaurant });
 });
 
 //** Update a detail of a restaurant
 //# 1. If restaurant ID did not exist or invalid, return 404 not found
 //# 2. If the given information did not pass the validation, return 400 bad requests
 app.put("/restaurants/:id", async (req: Request, res: Response) => {
-  const { id } = req.params as { id: string };
-  const { name, mapUrl } = req.body as { name: string; mapUrl: string };
+  const { id } = req.params;
+  const { name, mapUrl, notes } = req.body;
 
-  if (typeof name !== "string" || typeof mapUrl !== "string") {
-    return res.status(400).json({ message: "Could not update restaurant" });
+  const isNameExist = await validateIsExist({ name });
+  if (!validateParams(req.body) || isNameExist) {
+    return res.status(400).json();
   }
 
-  const updatedRestaurant = await prisma.restaurant.update({
-    where: {
-      id: id,
-    },
-    data: {
-      name,
-      mapUrl,
-    },
-  });
-
-  if (!updatedRestaurant) {
-    return res.status(404).json({ message: "Could not find restaurant" });
+  const isIdExist = await validateIsExist({ id });
+  if (isIdExist) {
+    const updatedRestaurant = await prisma.restaurant.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        mapUrl,
+        notes,
+      },
+    });
+    res.status(200).json({ data: updatedRestaurant });
+  } else {
+    return res.status(404).json();
   }
-
-  res.status(200).json({ data: updatedRestaurant });
 });
 
 //** Delete a restaurant
 //# 1. If restaurant ID did not exist or invalid, return 404 not found
 //# 2. Return 204 no content, if successfully deleted a restaurant record
 app.delete("/restaurants/:id", async (req: Request, res: Response) => {
-  const { id } = req.params as { id: string };
-  if (!id) {
-    return res.status(404).json({ message: "Could not find restaurant" });
-  }
-  res.status(204).json(
+  const { id } = req.params;
+
+  const isIdExist = await validateIsExist({ id });
+
+  if (isIdExist) {
     await prisma.restaurant.delete({
       where: {
-        id: id,
+        id,
       },
-    }),
-  );
+    });
+    return res.status(204).json();
+  } else {
+    return res.status(404).json();
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
